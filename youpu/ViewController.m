2a59d2aa4e1d6aa4be5e14f88eabc2536d1a2b4b
zhangwenfeng <santcool@126.com>
2014-10-30 00:19:08 +0800

    sdfa

--- youpu/ViewController.m
+++ youpu/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     NSLog(@"%@",self.view);
     NSLog(@"%@",self.view);
+    NSLog(@"%@",self.view);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
